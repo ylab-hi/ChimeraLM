102dbb951311ba8d066db6bbf0366068dbe3a7ce
Yangyang Li <yangyang.li@northwestern.edu>
2024-11-12 16:46:39 -0600

    update dependecies

--- src/bin/query.rs
+++ src/bin/query.rs
@@ -1,45 +1,26 @@
-//! Queries a BAM file with a given region.
+//! Queries a chimeric event
 //!
 //! The input BAM must have an index in the same directory.
 //!
 //! The result matches the output of `samtools view <src> <region>`.
 
-use std::{env, io};
+use ahash::HashMap;
+use anyhow::Result;
+use clap::Parser;
+use deepbiop::bam::chimeric::ChimericEvent;
+use deepbiop::utils::interval::{GenomicInterval, Overlap};
+use itertools::Itertools;
+use log::debug;
+use log::info;
+use rayon::prelude::*;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 use deepbiop::bam::chimeric::is_chimeric_record;
 use noodles::bam;
 use noodles::sam;
-use noodles::sam::alignment::io::Write;
 
-const UNMAPPED: &str = "*";
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut args = env::args();
-
-    let src = args.nth(1).expect("missing src");
-    let raw_region = args.next().expect("missing region");
-
-    let mut reader = bam::io::indexed_reader::Builder::default().build_from_path(src)?;
-    let header = reader.read_header()?;
-
-    let records: Box<dyn Iterator<Item = io::Result<bam::Record>>> = if raw_region == UNMAPPED {
-        reader.query_unmapped().map(Box::new)?
-    } else {
-        let region = raw_region.parse()?;
-        reader.query(&header, &region).map(Box::new)?
-    };
-
-    let stdout = io::stdout().lock();
-    let mut writer = sam::io::Writer::new(stdout);
-
-    for result in records {
-        let record = result?;
-        if is_chimeric_record(&record) {
-            writer.write_alignment_record(&header, &record)?;
-        }
-    }
-
-    writer.finish(&header)?;
-
+fn main() -> Result<()> {
+    println!("Hello, world!");
     Ok(())
 }
