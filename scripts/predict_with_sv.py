76cf2b25683fb4c4c635a6640cd7a154c449dbac
Yangyang Li <yangyang.li@northwestern.edu>
2025-02-21 10:51:00 -0600

    feat: Add script for predicting with structural variants

---
+++ scripts/predict_with_sv.py
@@ -0,0 +1,35 @@
+import chimera 
+import fire 
+
+
+def load_predict(path):
+    predicts  = chimera.load_predicts_from_batch_pts(path)
+    print(f"predicts: {len(predicts)}")
+    return predicts
+
+
+def load_sv(path):
+    res = {}
+    with open (path, 'r') as f:
+        for line in f:
+            read, sv = line.strip().split('\t')
+            res[read] = sv
+    return res
+
+
+
+def main(predict_path, sv_path):
+    predicts = load_predict(predict_path)
+    sv = load_sv(sv_path)
+    print (f"sv: {len(sv)}")
+
+    with open("predict_with_sv.text", 'w') as f:
+        for read, predict in predicts.items():
+            if read in sv:
+                f.write(f"{read}\t{predict}\t{sv[read]}\n")
+            else:
+                f.write(f"{read}\t{predict}\tNA\n")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
