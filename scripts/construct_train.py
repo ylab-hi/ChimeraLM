0eb8bac7152a008eb5b31b0dae8eb4f5a0c0a86d
Yangyang Li <yangyang.li@northwestern.edu>
2025-06-16 15:03:37 -0500

    feat: Enhance alignment segment handling and add chimeric read comparison functionality

--- scripts/construct_train.py
+++ scripts/construct_train.py
@@ -1,12 +1,16 @@
-from typing import Generator
 from dataclasses import dataclass
+
+import aligntools
 import pysam
+import typer
 from aligntools import Cigar
+from rich import print
 
 # 'chr1,3929928,+,2S892M24D2062S,60,57;chr1,3929454,-,14S1217M119D1725S,19,363;'
 
 @dataclass
 class AlignmentSegment:
+    """Alignment segment."""
     chromosome: str
     reference_start: int
     reference_end: int
@@ -16,6 +20,7 @@ class AlignmentSegment:
 
     @classmethod
     def from_str(cls, sa_part: str, read_name: str) -> 'AlignmentSegment':
+        """Create an alignment segment from a string."""
         # Split the sa tag
         sa_part_parts = sa_part.split(',')
         chromosome = sa_part_parts[0]
@@ -24,11 +29,39 @@ class AlignmentSegment:
         cigar = Cigar.coerce(sa_part_parts[3])
         reference_end = cigar.ref_length + reference_start
         return cls(chromosome, reference_start, reference_end, strand, read_name, cigar)
-    
+
+    @classmethod
+    def from_pysam(cls, alignment: pysam.AlignedSegment) -> 'AlignmentSegment':
+        """Create an alignment segment from a pysam alignment."""
+        if alignment.reference_name is None:
+            msg = f"Reference name is None for {alignment}"
+            raise ValueError(msg)
+        if alignment.query_name is None:
+            msg = f"Query name is None for {alignment}"
+            raise ValueError(msg)
+        if alignment.cigarstring is None:
+            msg = f"Cigarstring is None for {alignment}"
+            raise ValueError(msg)
+        if alignment.reference_end is None:
+            msg = f"Reference end is None for {alignment}"
+            raise ValueError(msg)
+
+        return cls(alignment.reference_name,
+         alignment.reference_start,
+         alignment.reference_end,
+         '+' if not alignment.is_reverse else '-',
+         alignment.query_name, Cigar.coerce(alignment.cigarstring))
+
+    def __str__(self) -> str:
+        """String representation of the alignment segment."""
+        return f"{self.__class__.__name__}({self.chromosome}:{self.reference_start}-{self.reference_end} {self.strand} {self.read_name})"
+
+    def __repr__(self) -> str:
+        """Representation of the alignment segment."""
+        return self.__str__()
 
 def is_primary_chimeric(alignment: pysam.AlignedSegment) -> bool:
-    """
-    Check if an alignment is a primary chimeric alignment.
+    """Check if an alignment is a primary chimeric alignment.
 
     Args:
         alignment (pysam.AlignedSegment): The alignment to check.
@@ -37,15 +70,10 @@ def is_primary_chimeric(alignment: pysam.AlignedSegment) -> bool:
         bool: _description_
     """
     # Check if the alignment has sa tag
-
-    if alignment.has_tag('sa'):
-        if not (alignment.is_secondary or alignment.is_supplementary):
-            return True
-    return False
+    return bool(alignment.has_tag('SA') and not (alignment.is_secondary or alignment.is_supplementary))
 
 def get_primary_chimeric_alignments(bam_file: str) -> dict[str, list[AlignmentSegment]]:
-    """
-    Get all primary chimeric alignments from a BAM file.
+    """Get all primary chimeric alignments from a BAM file.
 
     Args:
         bam_file (str): The path to the BAM file.
@@ -57,41 +85,69 @@ def get_primary_chimeric_alignments(bam_file: str) -> dict[str, list[AlignmentSe
         if is_primary_chimeric(alignment):
             read_name = alignment.query_name
             if read_name is None:
-                raise ValueError(f"Read name is None for {alignment}")
-            chimeric_reads[read_name] = list(chimeric_read_to_alignment_segments(alignment))
-
+                msg = f"Read name is None for {alignment}"
+                raise ValueError(msg)
+            chimeric_reads[read_name] = chimeric_read_to_alignment_segments(alignment)
     return chimeric_reads
 
+def sort_alignement_seg_from_one_read(alignment_segments: list[AlignmentSegment]) -> list[AlignmentSegment]:
+    """Sort alignment segments from one read."""
+    # check if has same chromosome
+    # if so, sort by reference start
+    # if not, sort by chromosome
+    # if len(alignment_segments) > 2, return original list
+    if len(alignment_segments) > 2:
+        return alignment_segments
 
-def chimeric_read_to_alignment_segments(chimeric_read: pysam.AlignedSegment) -> Generator[AlignmentSegment, None, None]:
+    if len(alignment_segments) == 2:
+        if alignment_segments[0].chromosome == alignment_segments[1].chromosome:
+            return sorted(alignment_segments, key=lambda x: x.reference_start)
+        return sorted(alignment_segments, key=lambda x: x.chromosome)
+    return alignment_segments
+
+def chimeric_read_to_alignment_segments(chimeric_read: pysam.AlignedSegment) -> list[AlignmentSegment]:
+    """Convert a chimeric read to a list of alignment segments."""
     # Get the sa tag
-    sa = chimeric_read.get_tag('sa')
+    sa = chimeric_read.get_tag('SA')
     if sa is None or not isinstance(sa, str):
-        raise ValueError(f"SA tag is None for {chimeric_read}")
+        msg = f"SA tag is None for {chimeric_read}"
+        raise ValueError(msg)
 
     # Get the read name
     read_name = chimeric_read.query_name
     if read_name is None:
-        raise ValueError(f"Read name is None for {chimeric_read}")
+        msg = f"Read name is None for {chimeric_read}"
+        raise ValueError(msg)
+
+    alignment_segments = []
+    alignment_segments.append(AlignmentSegment.from_pysam(chimeric_read))
 
     # Split the sa tag
     sa_parts = sa.split(';')
     for sa_part in sa_parts:
+        if sa_part == '':
+            continue
         alignment_segment = AlignmentSegment.from_str(sa_part, read_name)
-        yield alignment_segment
+        alignment_segments.append(alignment_segment)
 
+    return sort_alignement_seg_from_one_read(alignment_segments)
 
 def compare_alignment_segments(alignment_segment1: list[AlignmentSegment], alignment_segment2: list[AlignmentSegment]) -> bool:
-    """ Compare two alignment segments. """
+    """Compare two alignment segments."""
     if len(alignment_segment1) != len(alignment_segment2):
         return False
-    
-    if len(alignment_segment1) > 2 or len(alignment_segment2) > 2:
-        return False
-    
-    return True
 
-     
+    return not (len(alignment_segment1) > 2 or len(alignment_segment2) > 2)
+
+app = typer.Typer()
+
+@app.command()
+def compare_chimeric(bam_file: str):
+    """Compare chimeric reads."""
+    chimeric_reads = get_primary_chimeric_alignments(bam_file)
+    for _, alignment_segments in chimeric_reads.items():
+        print(alignment_segments)
+
 
 if __name__ == "__main__":
-    pass
+    app()
