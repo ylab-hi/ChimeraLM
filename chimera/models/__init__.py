92f8a15307767300b18a54114caeda40c8a3e183
Yangyang Li <yangyang.li@northwestern.edu>
2025-06-25 11:31:52 -0500

    refactor: include hyena component in model exports

--- chimera/models/__init__.py
+++ chimera/models/__init__.py
@@ -1,5 +1,5 @@
 """Model components for models."""
 
-from .components import cnn, mamba, striped_hyena, transformer
+from .components import cnn, hyena, mamba, striped_hyena, transformer
 
-__all__ = ["cnn", "mamba", "striped_hyena", "transformer"]
+__all__ = ["cnn", "hyena", "mamba", "striped_hyena", "transformer"]
