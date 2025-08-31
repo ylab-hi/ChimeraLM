bc5efb84854ce32006765abb53471af4488146a4
Yangyang Li <yangyang.li@northwestern.edu>
2025-08-31 14:28:05 -0500

    refactor: remove Predict module and related functions from the Rust implementation, streamlining the codebase

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,1 @@
-pub mod predict;
 pub mod python;
--- src/python.rs
+++ src/python.rs
@@ -1,24 +1,4 @@
-use crate::predict::Predict;
-use crate::predict::load_predicts_from_batch_pt;
-use crate::predict::load_predicts_from_batch_pts;
-use crate::predict::write_predicts_to_file;
-use deepbiop::utils;
-use log::{debug, error, info, warn};
 use pyo3::prelude::*;
-
-#[pyfunction]
-fn test_deepbiop() {
-    let segment = utils::interval::GenomicInterval::new("chr2", 100, 2000).unwrap();
-    println!("{segment:?}");
-}
-
-#[pyfunction]
-fn test_log() {
-    debug!("debug Hello from Rust!");
-    info!("info Hello from Rust!");
-    warn!("warn Hello from Rust!");
-    error!("error Hello from Rust!");
-}
 
 #[pyfunction]
 fn add(a: i32, b: i32) -> i32 {
@@ -29,13 +9,6 @@ fn add(a: i32, b: i32) -> i32 {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn chimera(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(test_log, m)?)?;
     m.add_function(wrap_pyfunction!(add, m)?)?;
-    m.add_function(wrap_pyfunction!(test_deepbiop, m)?)?;
-
-    m.add_class::<Predict>()?;
-    m.add_function(wrap_pyfunction!(load_predicts_from_batch_pt, m)?)?;
-    m.add_function(wrap_pyfunction!(load_predicts_from_batch_pts, m)?)?;
-    m.add_function(wrap_pyfunction!(write_predicts_to_file, m)?)?;
     Ok(())
 }
